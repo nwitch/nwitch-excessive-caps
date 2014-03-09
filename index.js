9ba1d39d0a213f25ecb4d9f34ec832e334601306
Kenan Yildirim <kenan@kenany.me>
2014-03-09 15:01:08 -0400

    Update for nwitch@0.4.0

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-var inherits = require('util').inherits;
 var capsRate = require('caps-rate');
 
-module.exports = function(NwitchPlugin) {
-  function Plugin(bot, client) {
-    NwitchPlugin.apply(this, arguments);
+function plugin() {
+  return function(irc){
+    irc.on('message', function(evt) {
+      var from = evt.from;
+      var to = evt.to;
+      var message = evt.message;
 
-    this.metadata = {
-      name: 'excessive-caps',
-      title: 'Excessive caps',
-      description: 'nwitch plugin for blocking screams',
-      version: '0.0.1',
-      author: 'Kenan Yildirim'
-    };
-
-    client.addListener('message', function(from, to, message) {
       if (message.length > 5 && capsRate(message) >= 0.55) {
         var destination = to.charAt(0) === '#' ? to : from;
-        client.say(destination, '.timeout ' + from + ' 2');
-        client.say(destination, 'STOP SCREAMING ' + from + '!');
+        irc.send(destination, '.timeout ' + from + ' 2');
+        irc.send(destination, 'STOP SCREAMING ' + from + '!');
       }
     });
-  }
+  };
+}
 
-  inherits(Plugin, NwitchPlugin);
-
-  return Plugin;
-};
+module.exports = plugin;
